aadb37edc92044e86b4ea171c9758294ad4c158b
Tylor Shin <shincode@icloud.com>
2017-07-28 22:02:09 +0900

    Added root component & Rendered to actual html page

---
+++ app/app.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+class App extends React.PureComponent {
+  render() {
+    return <div>hello world</div>;
+  }
+}
+
+export default App;
--- app/index.js
+++ app/index.js
@@ -1,1 +1,9 @@
-console.log('hello foobar');
+import * as Immutable from "immutable";
+import * as React from "react";
+import * as ReactDom from "react-dom";
+import App from './app';
+
+ReactDom.render(
+  <App />,
+  document.getElementById("react-app"),
+);
