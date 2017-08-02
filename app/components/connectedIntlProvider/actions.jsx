740f7d6a819b41fa3a90c560ef5716f242dd6560
Tylor Shin <shincode@icloud.com>
2017-08-02 22:34:35 +0900

    React intl (#4)
    * Fixed wrong intl logic
    
    * Updated immutable version

--- app/components/connectedIntlProvider/actions.jsx
+++ app/components/connectedIntlProvider/actions.jsx
@@ -1,27 +1,17 @@
 export const ACTION_TYPES = {
   CHANGE_LOCALE: 'LOCALE.CHANGE_LOCALE',
-  CHANGE_MESSAGE: 'LOCALE.CHANGE_MESSAGE',
 };
 
 /**
  *
  * @param { locale } string - locale code (ex: en)
+ * @param { messages } object - locale code
  */
-export function changeLocale(locale) {
+export function changeLocale(locale, messages) {
   return {
     type: ACTION_TYPES.CHANGE_LOCALE,
-    lang: locale,
-  };
-}
-
-/**
- *
- * @param { messages } object - locale code (ex: en)
- */
-export function changeMessages(messages) {
-  return {
-    type: ACTION_TYPES.CHANGE_MESSAGE,
     payload: {
+      lang: locale,
       messages,
     },
   };
--- app/components/connectedIntlProvider/reducer.jsx
+++ app/components/connectedIntlProvider/reducer.jsx
@@ -8,13 +8,8 @@ export const LOCALE_INITIAL_STATE = {
 
 export function reducer(state = LOCALE_INITIAL_STATE, action) {
   switch (action.type) {
-    case ACTION_TYPES.CHANGE_EMAIL_INPUT: {
-
-      return { ...state, ...{ lang: action.payload.lang } };
-    }
-
-    case ACTION_TYPES.CHANGE_MESSAGE: {
-      return { ...state, ...{ messages: action.payload.messages } };
+    case ACTION_TYPES.CHANGE_LOCALE: {
+      return { ...state, ...{ lang: action.payload.lang, messages: action.payload.messages } };
     }
 
     default:
