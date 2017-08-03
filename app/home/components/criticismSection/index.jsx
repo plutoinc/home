148bc8131f391347f6f3751e6be319dcddd26fa3
Jung Hwan Woo <junghwan@pluto.network>
2017-08-03 23:44:23 +0900

    Modify CriticismSection

--- app/home/components/criticismSection/index.jsx
+++ app/home/components/criticismSection/index.jsx
@@ -7,7 +7,7 @@ import withStyles from 'isomorphic-style-loader/lib/withStyles';
 const CriticismSection = ({ intl }) => {
     return (
       <div className={styles.criticismSection}>
-        <div className="container">
+        <div className={`container ${styles.innerContainer}`}>
           <h1 className={styles.headline}>
             {intl.formatMessage({ id: 'criticismSection.title' })}
           </h1>
@@ -15,21 +15,21 @@ const CriticismSection = ({ intl }) => {
             {intl.formatMessage({ id: 'criticismSection.subTitle' })}
           </h2>
           <ul className={`row ${styles.cardItemWrapper}`}>
-            <li className={`col-md-4 ${styles.card}`}>
+            <li className={`col-md-4 col-xs-12 ${styles.card}`}>
               <Icon className={styles.cardItemIcon} icon="INCREASING_REVENUE" />
               <div className={styles.cardTitle}>{intl.formatMessage({ id: 'criticismSection.cardTitle1' })}</div>
               <div className={styles.cardDescription}>
                 {intl.formatMessage({ id: 'criticismSection.cardDescription1' })}
               </div>
             </li>
-            <li className={`col-md-4 ${styles.card}`}>
+            <li className={`col-md-4 col-xs-12 ${styles.card}`}>
               <Icon className={styles.cardItemIcon} icon="RACK_OF_TRUST" />
               <div className={styles.cardTitle}>{intl.formatMessage({ id: 'criticismSection.cardTitle2' })}</div>
               <div className={styles.cardDescription}>
                 {intl.formatMessage({ id: 'criticismSection.cardDescription2' })}
               </div>
             </li>
-            <li className={`col-md-4 ${styles.card}`}>
+            <li className={`col-md-4 col-xs-12 ${styles.card}`}>
               <Icon className={styles.cardItemIcon} icon="RESOURCE_COST" />
               <div className={styles.cardTitle}>{intl.formatMessage({ id: 'criticismSection.cardTitle3' })}</div>
               <div className={styles.cardDescription}>
