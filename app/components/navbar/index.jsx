17f6f5621d606b8f5e00f7fc3dcc58fbf779ed1d
Jung Hwan Woo <junghwan@pluto.network>
2017-08-22 00:45:19 +0900

    Add language change button design

--- app/components/navbar/index.jsx
+++ app/components/navbar/index.jsx
@@ -4,21 +4,25 @@ import { Link } from 'react-router';
 import styles from './navbar.scss';
 import withStyles from 'isomorphic-style-loader/lib/withStyles';
 import Icon from '../icons';
+// change lang action
+import { changeLocale } from '../connectedIntlProvider/actions';
 
 class Navbar extends React.PureComponent {
   constructor(props) {
     super(props);
 
     this.state = {
-      isOpen: false,
+      isMobileOpen: false,
+      isLangOpen: false,
     };
   }
 
   render() {
     const { intl, isTop } = this.props;
+    const localeIsEn = intl.locale === 'en';
 
     return (
-      <nav className={`${styles.navbar} ${isTop ? styles.transparent : ''} ${this.state.isOpen ? styles.isOpen : ''}`}>
+      <nav className={`${styles.navbar} ${isTop ? styles.transparent : ''} ${this.state.isMobileOpen ? styles.isMobileOpen : ''}`}>
         <div className={styles.container}>
           <Link className={styles.logo} to="/" >
             <Icon icon="PLUTO" />
@@ -26,15 +30,15 @@ class Navbar extends React.PureComponent {
           <div
             className={styles.mobileBtn}
             onClick={() => {
-              const curIsOpen = this.state.isOpen;
-              this.setState({ isOpen: !curIsOpen });
+              const d = this.state.isMobileOpen;
+              this.setState({ isMobileOpen: !d });
             }}
           >
             <Icon icon="MOBILE_BTN" className={styles.mobileIcon} />
             <Icon icon="CANCEL" className={styles.cancelIcon} />
           </div>
           <ul
-            className={`${styles.menuList} ${this.state.isOpen ? styles.isOpen : ''}`}
+            className={`${styles.menuList} ${this.state.isMobileOpen ? styles.isMobileOpen : ''}`}
           >
             <li>
               <Link className={styles.menuItem} to="/">
@@ -56,6 +60,31 @@ class Navbar extends React.PureComponent {
                 {intl.formatMessage({ id: 'NAVBAR.contribute' })}
               </Link>
             </li> */}
+            <li className={styles.langItem}>
+              <div
+                className={styles.langBtn}
+                onClick={() => {
+                  const curOpen = this.state.isLangOpen;
+                  this.setState({ isLangOpen: !curOpen });
+                }}
+              >
+                <img src="https://d2vo77dayzjoat.cloudfront.net/language-change.png" />
+              </div>
+              <ul className={`${styles.langList} ${this.state.isLangOpen ? styles.isLangOpen : ''}`}>
+                <li
+                  className={localeIsEn ? styles.active : ''}
+                  onClick={() => console.log('sdf')}
+                >
+                  EN
+                </li>
+                <li
+                  className={localeIsEn ? '' : styles.active}
+                  onClick={() => console.log('asdf')}
+                >
+                  KO
+                </li>
+              </ul>
+            </li>
           </ul>
         </div>
       </nav>
