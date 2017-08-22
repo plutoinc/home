f39f0d7798a993d4e7d7bd671d18da1541af656f
Tylor Shin <shincode@icloud.com>
2017-08-22 20:30:58 +0900

    Fix wrong intl detecting logic & add intl change button

--- app/components/connectedIntlProvider/actions.jsx
+++ app/components/connectedIntlProvider/actions.jsx
@@ -1,6 +1,36 @@
+import { addLocaleData } from "react-intl";
+import enLocaleData from "react-intl/locale-data/en";
+import koLocaleData from "react-intl/locale-data/ko";
+
+export const SUPPORTED_LANGUAGES = ["en", "ko"];
+export const DEFAULT_LANGUAGE = "en";
+
 export const ACTION_TYPES = {
-  CHANGE_LOCALE: 'LOCALE.CHANGE_LOCALE',
+  CHANGE_LOCALE: "LOCALE.CHANGE_LOCALE",
 };
+
+const TRANSLATIONS = {};
+for (const locale of SUPPORTED_LANGUAGES) {
+  TRANSLATIONS[locale] = require(`./assets/${locale}.json`);
+}
+
+export function addLocaleDataSet() {
+  const allLocaleData = [...enLocaleData, ...koLocaleData];
+  addLocaleData(allLocaleData);
+}
+
+/**
+ *
+ * @param { locale } string - locale code (ex: en)
+ */
+
+export function getMessages(locale) {
+  let localeData = TRANSLATIONS[locale];
+  if (!localeData) {
+    localeData = TRANSLATIONS[DEFAULT_LANGUAGE];
+  }
+  return localeData;
+}
 
 /**
  *
