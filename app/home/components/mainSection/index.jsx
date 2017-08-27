17db5add84bb05911e039239ad7dc17eb8e95d76
Jung Hwan Woo <junghwan@pluto.network>
2017-08-27 17:01:05 +0900

    Change mainSection background image

--- app/home/components/mainSection/index.jsx
+++ app/home/components/mainSection/index.jsx
@@ -2,18 +2,16 @@ import React from 'react';
 // styles
 import styles from './mainSection.scss';
 import withStyles from 'isomorphic-style-loader/lib/withStyles';
-import Icon from '../../../components/icons';
 
 const MainSection = ({ intl, email, subscribeEmail, handleEmailChange, mainRef }) => (
   <div className={styles.mainSectionContainer} ref={ref => { mainRef(ref); }}>
     <div className={`container ${styles.innerContainer}`}>
       <div>
         <div className="row">
-          <div className={`col-md-12 col-xs-12 ${styles.headline}`}>
-              Decentralized Scholarly Communication
-              Platform, Pluto
+          <div className={styles.headline}>
+              <span>Decentralized</span>Scholarly Communication Platform
             </div>
-          <div className={`col-md-12 col-xs-12 ${styles.subTitle}`}>
+          <div className={styles.subTitle}>
             {intl.formatMessage({ id: 'MAIN_SECTION.subtitle' })}
           </div>
         </div>
@@ -33,21 +31,6 @@ const MainSection = ({ intl, email, subscribeEmail, handleEmailChange, mainRef }
             {intl.formatMessage({ id: 'MAIN_SECTION.subscribe' })}
           </button>
         </form>
-
-        <div className={styles.iconsWrapper}>
-          <a href="https://github.com/pluto-net" target="_blank" className={styles.icon}>
-            <Icon icon="GITHUB_WHITE" />
-          </a>
-          {/* <a href="/" className={styles.icon}>
-            <Icon icon="REDDIT_WHITE" />
-          </a> */}
-          <a href="https://twitter.com/pluto_network" target="_blank" className={styles.icon}>
-            <Icon icon="TWITTER_WHITE" />
-          </a>
-          <a href="https://medium.com/pluto-network" target="_blank" className={styles.icon}>
-            <Icon icon="MEDIUM_WHITE" />
-          </a>
-        </div>
       </div>
     </div>
   </div>
