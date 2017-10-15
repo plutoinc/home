a36f1c1985e3e8f9f455c877f5d4c1ffed811c5f
Jung Hwan Woo <junghwan@pluto.network>
2017-10-15 15:58:49 +0900

    Make videoSection and install modal package

--- app/newhome/components/videoSection/index.jsx
+++ app/newhome/components/videoSection/index.jsx
@@ -1,16 +1,37 @@
 import React from 'react';
+import ModalVideo from 'react-modal-video';
 
 // styles
 import styles from "./videoSection.scss";
 import withStyles from "isomorphic-style-loader/lib/withStyles";
 import Icon from "../../../components/icons";
 
-const VideoSection = () => (
-  <section className={styles.videoSectionContainer}>
-    <div className={styles.innerContainer}>
-      videoSection
-    </div>
-  </section>
-);
+class VideoSection extends React.PureComponent {
+  constructor() {
+    super();
+    this.state = {
+      isVideoOpen: false,
+    };
+    this.openVideoModal = this.openVideoModal.bind(this);
+  }
+
+  render () {
+    return (
+      <section className={styles.videoSectionContainer}>
+        <div className={styles.innerContainer}>
+          <ModalVideo channel='youtube' isOpen={this.state.isVideoOpen} videoId='7wtfhZwyrcc' onClose={() => this.setState({isVideoOpen: false})} />
+          <div className={styles.videoBtn} onClick={this.openVideoModal}>
+            <img src="https://img.youtube.com/vi/7wtfhZwyrcc/maxresdefault.jpg" alt=""/>
+            <span className={styles.playBtn}>PLAY</span>
+          </div>
+        </div>
+      </section>
+    );
+  }
+
+  openVideoModal () {
+    this.setState({ isVideoOpen: true });
+  }
+}
 
 export default withStyles(styles)(VideoSection);
