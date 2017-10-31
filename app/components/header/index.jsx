a818e4ea931d76efd45632ce08bc3ca0827a7ad5
Jung Hwan Woo <junghwan@pluto.network>
2017-10-31 10:17:01 +0900

    Fix header background

--- app/components/header/index.jsx
+++ app/components/header/index.jsx
@@ -20,9 +20,9 @@ class Header extends React.PureComponent {
     const { isTop } = this.props;
     return (
       <header
-        className={`${styles.headerContainer} ${this.state.isMenuOpen ? styles.isOpen : ""} ${
-          isTop ? styles.transparent : ""
-        }`}
+        className={`${styles.headerContainer} ${this.state.isMenuOpen ? styles.isOpen : ""} ${isTop
+          ? styles.transparent
+          : ""}`}
       >
         <div
           className={`${styles.menuListOverlay} ${this.state.isMenuOpen ? styles.isOpen : ""}`}
@@ -53,8 +53,7 @@ class Header extends React.PureComponent {
             <li className={`${styles.menuItem} ${styles.whitePaperItem}`}>
               <a
                 onClick={() =>
-                  trackAndOpenLink("https://d2vo77dayzjoat.cloudfront.net/PLUTO_whitepaper_ver_0.2.pdf", "Header")
-                }
+                  trackAndOpenLink("https://d2vo77dayzjoat.cloudfront.net/PLUTO_whitepaper_ver_0.2.pdf", "Header")}
               >
                 White Paper
               </a>
