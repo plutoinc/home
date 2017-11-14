558b1a022fa88ff4bca967745ce04e0a1e46cd95
Jung Hwan Woo <junghwan@pluto.network>
2017-11-14 10:29:26 +0900

    Close other questions

--- app/faq/components/answerSection/answerItem/index.jsx
+++ app/faq/components/answerSection/answerItem/index.jsx
@@ -3,36 +3,17 @@ import React from "react";
 import withStyles from "isomorphic-style-loader/lib/withStyles";
 import styles from "./answerItem.scss";
 
-class AnswerItem extends React.PureComponent {
-  constructor(props) {
-    super(props);
-
-    this.state = {
-      isOpen: this.props.isOpen,
-    };
-  }
-
-  render() {
-    const isOpen = this.state.isOpen;
-
-    return (
-      <div className={`${styles.answerItem} ${isOpen ? styles.open : ""}`}>
-        <div className={styles.itemContainer}>
-          <div
-            className={styles.itemTitle}
-            onClick={() => {
-              this.setState({
-                isOpen: !this.state.isOpen,
-              });
-            }}
-          >
-            {this.props.question}
-          </div>
-          <div className={styles.itemContent}>{this.props.answer}</div>
+const AnswerItem = ({ isOpen, selectAnswerItem, index, question, answer }) => {
+  return (
+    <div className={`${styles.answerItem} ${isOpen ? styles.open : ""}`}>
+      <div className={styles.itemContainer}>
+        <div className={styles.itemTitle} onClick={() => selectAnswerItem(index)}>
+          {question}
         </div>
+        <div className={styles.itemContent}>{answer}</div>
       </div>
-    );
-  }
-}
+    </div>
+  );
+};
 
 export default withStyles(styles)(AnswerItem);
--- app/faq/components/answerSection/index.jsx
+++ app/faq/components/answerSection/index.jsx
@@ -6,16 +6,41 @@ import faqContent from "./faqContent";
 import withStyles from "isomorphic-style-loader/lib/withStyles";
 import styles from "./answerSection.scss";
 
-const AnswerSection = () => {
-  return (
-    <section className={styles.answerSectionContainer}>
-      <div className={styles.innerContainer}>
-        {faqContent.map((item, idx) => {
-          return <AnswerItem isOpen={idx === 0} question={item.question} answer={item.answer} />;
-        })}
-      </div>
-    </section>
-  );
-};
+class AnswerSection extends React.PureComponent {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      selected: 0,
+    };
+
+    this.selectAnswerItem = this.selectAnswerItem.bind(this);
+  }
+
+  render() {
+    return (
+      <section className={styles.answerSectionContainer}>
+        <div className={styles.innerContainer}>
+          {faqContent.map((item, idx) => {
+            return (
+              <AnswerItem
+                key={idx.toString()}
+                index={idx}
+                isOpen={idx === this.state.selected}
+                question={item.question}
+                answer={item.answer}
+                selectAnswerItem={this.selectAnswerItem}
+              />
+            );
+          })}
+        </div>
+      </section>
+    );
+  }
+
+  selectAnswerItem(idx) {
+    this.setState({ selected: idx });
+  }
+}
 
 export default withStyles(styles)(AnswerSection);
