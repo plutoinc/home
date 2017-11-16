9e5a529b12ccac8fadfc3d13b5579fb39401bca0
Jung Hwan Woo <junghwan@pluto.network>
2017-11-16 19:02:00 +0900

    Change webfont to roboto

--- app/helpers/htmlWrapper.jsx
+++ app/helpers/htmlWrapper.jsx
@@ -1,9 +1,4 @@
-export function staticHTMLWrapper(
-  reactDom,
-  scriptPath,
-  initialState,
-  css,
-) {
+export function staticHTMLWrapper(reactDom, scriptPath, initialState, css) {
   return `
     <!doctype html>
     <html>
@@ -12,7 +7,6 @@ export function staticHTMLWrapper(
         <link rel="shortcut icon" href="https://dd2gn9pwu61vr.cloudfront.net/favicon.png" />
         <title>Pluto | Home</title>
         <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=0">
-        <link href='//spoqa.github.io/spoqa-han-sans/css/SpoqaHanSans-kr.css' rel='stylesheet' type='text/css'>
         <style type="text/css">${css}</style>
       </head>
       <body>
