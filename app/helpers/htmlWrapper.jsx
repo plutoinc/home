7ebd117fd3f15e241560c06293a32eeb69b1b797
Tylor Shin <shincode@icloud.com>
2018-01-26 15:10:06 +0900

    Add basic meta tag settings

--- app/helpers/htmlWrapper.jsx
+++ app/helpers/htmlWrapper.jsx
@@ -3,10 +3,32 @@ export function staticHTMLWrapper(reactDom, scriptPath, initialState, css) {
     <!doctype html>
     <html>
       <head>
-        <meta charset="utf-8">
-        <link rel="shortcut icon" href="https://dd2gn9pwu61vr.cloudfront.net/favicon.png" />
-        <title>Pluto | Home</title>
-        <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=0">
+      <meta charset="utf-8">
+      <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=0">
+      <link rel="shortcut icon" href="https://assets.pluto.network/favicon.png" />
+
+      <title>Pluto Network | Decentralied scholarly communication</title>
+      <meta name="description" content="A dedicated team building digital tools to break down barriers in academia, Pluto Network advances the way science evolves. Join the great march and empower the community."
+      />
+
+      <meta itemprop="name" content="Pluto Network | Decentralied scholarly communication">
+      <meta itemprop="description" content="A dedicated team building digital tools to break down barriers in academia, Pluto Network advances the way science evolves. Join the great march and empower the community.">
+      <meta itemprop="image" content="https://assets.pluto.network/pluto-home-meta.jpg">
+
+      <meta name="twitter:card" content="Pluto Network">
+      <meta name="twitter:site" content="@pluto_network">
+      <meta name="twitter:title" content="Pluto Network | Decentralied scholarly communication">
+      <meta name="twitter:description" content="A dedicated team building digital tools to break down barriers in academia, Pluto Network advances the way science evolves. Join the great march and empower the community.">
+      <meta name="twitter:creator" content="@pluto_network">
+      <meta name="twitter:image" content="https://assets.pluto.network/pluto-home-meta.jpg">
+
+      <meta property="og:title" content="Pluto Network | Decentralied scholarly communication" />
+      <meta property="og:type" content="article" />
+      <meta property="og:url" content="https://pluto.network" />
+      <meta property="og:image" content="https://assets.pluto.network/pluto-home-meta.jpg" />
+      <meta property="og:description" content="A dedicated team building digital tools to break down barriers in academia, Pluto Network advances the way science evolves. Join the great march and empower the community."
+      />
+      <meta property="og:site_name" content="Pluto Network" />
         <style type="text/css">${css}</style>
       </head>
       <body>
