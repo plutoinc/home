a2b676869c55d94b598b627440a8892b4468e76b
yukyeong <kykkjh2984@gmail.com>
2018-03-29 10:57:00 +0900

    Fix spelling decentralied to decentralized

--- app/helpers/htmlWrapper.jsx
+++ app/helpers/htmlWrapper.jsx
@@ -7,22 +7,22 @@ export function staticHTMLWrapper(reactDom, scriptPath, initialState, css) {
       <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=0">
       <link rel="shortcut icon" href="https://assets.pluto.network/favicon.png" />
 
-      <title>Pluto Network | Decentralied scholarly communication</title>
+      <title>Pluto Network | Decentralized scholarly communication</title>
       <meta name="description" content="A dedicated team building digital tools to break down barriers in academia, Pluto Network advances the way science evolves. Join the great march and empower the community."
       />
 
-      <meta itemprop="name" content="Pluto Network | Decentralied scholarly communication">
+      <meta itemprop="name" content="Pluto Network | Decentralized scholarly communication">
       <meta itemprop="description" content="A dedicated team building digital tools to break down barriers in academia, Pluto Network advances the way science evolves. Join the great march and empower the community.">
       <meta itemprop="image" content="https://assets.pluto.network/pluto-home-meta.jpg">
 
       <meta name="twitter:card" content="Pluto Network">
       <meta name="twitter:site" content="@pluto_network">
-      <meta name="twitter:title" content="Pluto Network | Decentralied scholarly communication">
+      <meta name="twitter:title" content="Pluto Network | Decentralized scholarly communication">
       <meta name="twitter:description" content="A dedicated team building digital tools to break down barriers in academia, Pluto Network advances the way science evolves. Join the great march and empower the community.">
       <meta name="twitter:creator" content="@pluto_network">
       <meta name="twitter:image" content="https://assets.pluto.network/pluto-home-meta.jpg">
 
-      <meta property="og:title" content="Pluto Network | Decentralied scholarly communication" />
+      <meta property="og:title" content="Pluto Network | Decentralized scholarly communication" />
       <meta property="og:type" content="article" />
       <meta property="og:url" content="https://pluto.network" />
       <meta property="og:image" content="https://assets.pluto.network/pluto-home-meta.jpg" />
