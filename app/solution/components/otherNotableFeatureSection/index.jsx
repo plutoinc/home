c63abeca0cbf33f3b3351ab7415f7db99765ff8d
NohYujeong <shdbwjd0326@naver.com>
2019-07-31 19:11:10 +0900

    WIP - solution container

--- app/solution/components/otherNotableFeatureSection/index.jsx
+++ app/solution/components/otherNotableFeatureSection/index.jsx
@@ -1,4 +1,5 @@
 import React from "react";
+import Icon from "../../../components/icons";
 // styles
 import styles from "./otherNotableFeatureSection.scss";
 import withStyles from "isomorphic-style-loader/lib/withStyles";
@@ -7,18 +8,64 @@ const OtherNotableFeatureSection = () => {
   return (
     <section className={styles.otherNotableFeatureSectionContainer}>
       <div className={styles.innerContainer}>
-        <div className={styles.subTitle}>COMPARE</div>
-        <div className={styles.title}>Interface & Paper Duplicates</div>
-        <div className={styles.contentDivider} />
-        <div className={styles.chartContainer}>
-          <div className={styles.chartWrapper}>
-            <img src="https://assets.pluto.network/pluto_team_hompage/compareChart%403x.png" />
+        <div className={styles.title}>Other notable features</div>
+        <div className={styles.contentWrapper}>
+          <div className={styles.featureItem}>
+            <Icon icon="CHECK_ICON" className={styles.checkIcon} />
+            <div className={styles.featureContext}>
+              User-friendly (Research-friendly) interface
+            </div>
           </div>
-          <div className={styles.focusArea} />
-        </div>
-        <div className={styles.smallContext}>
-          *duplicate results may occur due to error in metadata aggregation,
-          duplicate subscription, or indexing error
+          <div className={styles.featureItem}>
+            <Icon icon="CHECK_ICON" className={styles.checkIcon} />
+            <div className={styles.featureContext}>
+              Online support and online management of user data
+            </div>
+          </div>
+
+          <div className={styles.featureItem}>
+            <Icon icon="CHECK_ICON" className={styles.checkIcon} />
+            <div className={styles.featureContext}>
+              Supports semantic search / keyword search
+            </div>
+          </div>
+
+          <div className={styles.featureItem}>
+            <Icon icon="CHECK_ICON" className={styles.checkIcon} />
+            <div className={styles.featureContext}>Proxy service</div>
+          </div>
+          <div className={styles.featureItem}>
+            <Icon icon="CHECK_ICON" className={styles.checkIcon} />
+            <div className={styles.featureContext}>
+              Provide additional licensing resources and support
+            </div>
+          </div>
+          <div className={styles.featureItem}>
+            <Icon icon="CHECK_ICON" className={styles.checkIcon} />
+            <div className={styles.featureContext}>Support for consortium</div>
+          </div>
+          <div className={styles.featureItem}>
+            <Icon icon="CHECK_ICON" className={styles.checkIcon} />
+            <div className={styles.featureContext}>
+              Most relevant and efficient results based on citation count,
+              journal IF, Conferences tier, authors' h-index, published date
+            </div>
+          </div>
+          <div className={styles.featureItem}>
+            <Icon icon="CHECK_ICON" className={styles.checkIcon} />
+            <div className={styles.featureContext}>
+              Provide Open Access resources
+              <br />
+              (provide link to pre-prints of OA journals and non-oa journals)
+            </div>
+          </div>
+          <div className={styles.featureItem}>
+            <Icon icon="CHECK_ICON" className={styles.checkIcon} />
+            <div className={styles.featureContext}>
+              Provide preprint repositories available within
+              institution/university
+            </div>
+          </div>
         </div>
       </div>
     </section>
