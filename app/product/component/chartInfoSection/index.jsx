944b71cefb4f13cac28ef91411b45c5b3cfabc01
NohYujeong <shdbwjd0326@naver.com>
2019-08-01 12:21:16 +0900

    Apply chart info section mobile design

--- app/product/component/chartInfoSection/index.jsx
+++ app/product/component/chartInfoSection/index.jsx
@@ -20,27 +20,35 @@ const ChartInfoSection = () => {
           </div>
         </div>
         <div className={styles.graphContent}>
-          <img src="https://assets.pluto.network/pluto_team_hompage/graph%403x.png" />
           <div className={styles.graphDescription}>
             <div className={styles.description}>
-              <div className={styles.magnification}>↑ 4x</div>
-              <div className={styles.subDesc}>
-                We provide much fewer outdated and low-impact papers than Google
-                Scholar.
+              <img src="https://assets.pluto.network/pluto_team_hompage/graph-1%403x.png" />
+              <div className={styles.textWrapper}>
+                <div className={styles.magnification}>↑ 4x</div>
+                <div className={styles.subDesc}>
+                  We provide much fewer outdated and low-impact papers than
+                  Google Scholar.
+                </div>
               </div>
             </div>
             <div className={styles.description}>
-              <div className={styles.magnification}>↑ 1.4x</div>
-              <div className={styles.subDesc}>
-                We provide results with generally higher average citation counts
-                with similar published dates.
+              <img src="https://assets.pluto.network/pluto_team_hompage/graph-2%403x.png" />
+              <div className={styles.textWrapper}>
+                <div className={styles.magnification}>↑ 1.4x</div>
+                <div className={styles.subDesc}>
+                  We provide results with generally higher average citation
+                  counts with similar published dates.
+                </div>
               </div>
             </div>
             <div className={styles.description}>
-              <div className={styles.magnification}>↑ 1.5x</div>
-              <div className={styles.subDesc}>
-                We help bring out recently published papers in noteworthy
-                journals.
+              <img src="https://assets.pluto.network/pluto_team_hompage/graph-3%403x.png" />
+              <div className={styles.textWrapper}>
+                <div className={styles.magnification}>↑ 1.5x</div>
+                <div className={styles.subDesc}>
+                  We help bring out recently published papers in noteworthy
+                  journals.
+                </div>
               </div>
             </div>
           </div>
