c9ca9091ef509d2203cacd7139d1f9569331e15a
NohYujeong <shdbwjd0326@naver.com>
2019-08-02 14:13:58 +0900

    Change home main section and table image show logic

--- app/home/components/mainSection/index.jsx
+++ app/home/components/mainSection/index.jsx
@@ -1,7 +1,11 @@
 import React from "react";
 // styles
+import { Link } from "react-router";
 import styles from "./mainSection.scss";
 import withStyles from "isomorphic-style-loader/lib/withStyles";
+import { DefaultButton } from "../commonButton";
+import Icon from "../../../components/icons";
+import { PRODUCT_URL } from "../../../routes";
 
 const MainSection = params => {
   const { shown } = params;
@@ -9,10 +13,23 @@ const MainSection = params => {
     <section
       className={`${styles.mainSectionContainer} ${shown ? styles.shown : ""}`}
     >
-      <div className={styles.innerContainer}>
-        <div className={styles.title}>Rediscover knowledge</div>
-        <div className={styles.subTitle}>
-          Pluto, where new research begins...
+      <div className={styles.backgroundImg}>
+        <div className={styles.innerContainer}>
+          <div className={styles.title}>Rediscover knowledge</div>
+          <div className={styles.subTitle}>
+            Pluto, where new research begins...
+          </div>
+          <div className={styles.buttonWrapper}>
+            <DefaultButton
+              url={PRODUCT_URL}
+              text="Our Product"
+              from="mainSection"
+            />
+            <Link className={styles.learnMoreButton} to="/contact">
+              Contact Us
+              <Icon icon="ARROW_FORWARD" className={styles.forwardArrowIcon} />
+            </Link>
+          </div>
         </div>
       </div>
     </section>
